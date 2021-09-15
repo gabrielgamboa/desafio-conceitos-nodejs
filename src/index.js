7160aa089635db49c6309f7585bfee392174470c
gabrielgamboa <gabriel_gamboa10@hotmail.com>
2021-09-14 21:02:43 -0300

    feat: criando rotas para verificar e criar os todos

--- src/index.js
+++ src/index.js
@@ -1,29 +1,71 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  console.log(username);
+  const user = users.find(user => user.username === username);
+
+  if (user) {
+    request.user = user;
+    return next();
+  }
+
+  return response.status(400).json({error: 'Usuário não existe no sistena.'});
 }
 
+//criar um usuário
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const {name, username} = request.body;
+  const id = uuidv4();
+
+  const user = {
+    id,
+    name,
+    username,
+    todos: []
+  };
+
+  users.push(user);
+
+  return response.status(201).json(user);
 });
 
+//verificar todos através de um username
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const todos = user.todos;
+
+  return response.json(todos);
 });
 
+//criar um todo através do username
 app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const {title, deadline} = request.body;
+  const { user } = request;
+  const id = uuidv4();
+
+  const todo = {
+    id,
+    title,
+    done: false,
+    deadline: new Date(deadline),
+    created_at: new Date()
+  };
+
+  user.todos.push(todo);
+
+  return response.status(201).json(todo);
+
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
